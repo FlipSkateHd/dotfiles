655d11843f336f95b7345c851a0b63209a3facda
0x66 <117127775+FlipSkateHd@users.noreply.github.com>
2025-11-02 19:02:07 -0300

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,8 +5,31 @@ return {
     dependencies = "rktjmp/lush.nvim", -- opcional para mais configuração
     lazy = false,
     priority = 1000,
+    config = function() end,
+  },
+
+  {
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme("zenbones")
+      require("NeoSolarized").setup({
+        style = "dark", -- Modo dark
+        terminal_colors = true, -- Habilita cores pra terminal integrado
+        enable_italics = true, -- Itálico para comentários, keywords etc.
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = { bold = true },
+          string = { italic = true },
+          underline = true,
+          undercurl = true,
+        },
+      })
+      vim.cmd.colorscheme("NeoSolarized")
+
+      vim.api.nvim_set_hl(0, "VertSplit", { fg = "#505050" })
     end,
+    dependencies = { "tjdevries/colorbuddy.nvim" }, -- Necessário
   },
 }
